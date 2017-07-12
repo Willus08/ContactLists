0d813188b6bddce67537b97d7768ff28fbff77c2
Will <baldesw08@gmail.com>
2017-07-12 07:55:46 -0400

    calls work now

--- app/src/main/java/posidenpalace/com/contactlists/Contact.java
+++ app/src/main/java/posidenpalace/com/contactlists/Contact.java
@@ -10,6 +10,7 @@ import android.support.v4.app.ActivityCompat;
 import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.telecom.Call;
 import android.telephony.SmsManager;
 import android.view.View;
 import android.widget.EditText;
@@ -62,13 +63,16 @@ public class Contact extends AppCompatActivity  {
     }
 
     public void Call(View view) {
-        Uri num = Uri.parse("tel" + number);
-        Intent phpneInente = new Intent(Intent.ACTION_CALL);
-        phpneInente.setData(num);
+        Uri num = Uri.parse("tel:" + number);
+        Intent phoneIntent = new Intent(Intent.ACTION_CALL);
+        phoneIntent.setData(num);
         if (ActivityCompat.checkSelfPermission(this,
                 android.Manifest.permission.CALL_PHONE) != getPackageManager().PERMISSION_GRANTED) {
-            return;
+            ActivityCompat.requestPermissions(this,new String[]{Manifest.permission.CALL_PHONE},My_REQUEST);
         }
-        startActivity(phpneInente);
+        startActivity(phoneIntent);
+
     }
+
+
 }
